b83f30ca32fe9e5f87bbe0773d19aa0abd8791e1
Kevin Garay <kevingcfcb88@hotmail.com>
2021-05-19 10:26:31 -0600

    Core.Domain created

---
+++ Todos.Domain/Entities/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Todos.Domain.Common;
+
+namespace Todos.Domain.Entities
+{
+    public class Todo : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public TodoStatus TodoStatusId { get; set; }
+        public User UserId { get; set; }
+    }
+}
---
+++ Todos.Domain/Entities/TodoStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Todos.Domain.Common;
+
+namespace Todos.Domain.Entities
+{
+    public class TodoStatus : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}

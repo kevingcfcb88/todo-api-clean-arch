bccae354ce2c0fd800b7e60424971de6ec9c51b6
Kevin Garay <kevingcfcb88@hotmail.com>
2021-05-19 10:14:38 -0600

    Add project files.

---
+++ Todos.Domain/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todos.Domain.Common
+{
+    public class BaseEntity
+    {
+        public bool Status { get; set; }
+        public DateTime Created_at { get; set; }
+        public string Created_by { get; set; }
+        public DateTime Modified_at { get; set; }
+        public string Modified_by { get; set; }
+    }
+}
